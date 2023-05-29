163a5f1aec5ef1e160fa9f61503d54542c1ff9b1
mike <mike@vector5.ai>
2023-05-30 00:06:41 +0200

    initial commit

---
+++ src/metrics/CosineSimilarity.ts
@@ -0,0 +1,9 @@
+export class CosineSimilarity {
+    static distance(a: number[], b: number[]): number {
+        const dotProduct = a.reduce((acc, value, index) => acc + value * b[index], 0);
+        const magnitudeA = Math.sqrt(a.reduce((acc, value) => acc + Math.pow(value, 2), 0));
+        const magnitudeB = Math.sqrt(b.reduce((acc, value) => acc + Math.pow(value, 2), 0));
+
+        return 1 - (dotProduct / (magnitudeA * magnitudeB));
+    }
+}
